a769002f959ffa02f1f186c5f29ef6cb48560ee3
Alexandre <alexandre.trieste@gmail.com>
2021-10-12 15:48:09 -0300

    mensage error commit

--- index.php
+++ index.php
@@ -1,38 +1,40 @@
-<?php 
-$categoria = [];
-$categoria[] = "Infantil";
-$categoria[] = "Adolescente";
-$categoria[] = "Adulto";
-//$categoria = array("Adulto", "Adolescente", "Infantil");
-
-$nome = "Sheyla";
-
-$idade = 7;
-
-
-if ($idade >= 6 && $idade <= 12) 
-{
-    for($i = 0; $i <= count($categoria); $i++) 
-    { 
-        if($categoria[$i] == "Infantil") 
-        
-            echo "O nadador ", $nome, " compete na categoria ", $categoria[$i],"!    ";   
-    }
-}elseif ($idade >= 12 && $idade <= 18) 
-{
-    for($i = 0; $i <= count($categoria); $i++) 
-    { 
-        if($categoria[$i] == "Adolescente") 
-        
-            echo "O nadador ", $nome, " compete na categoria ", $categoria[$i],"!    ";   
-    }
-}else 
-{
-    for($i = 0; $i <= count($categoria); $i++) 
-    { 
-        if($categoria[$i] == "Adulto") 
-        
-            echo "O nadador ", $nome, " compete na categoria ", $categoria[$i],"!    ";   
-    }
-}
+<?php
+include "servicos/servicoMensagemSessao.php";
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+    <?php
+        $mensagemDeSucesso = obterMensagemSucesso();
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = obterMensagemErro();
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input type="text" name="idade" /></p>
+    <p><input type="submit" value="Enviar dados do competidor"/></p>
+</form>
+
+</body>
+
+</html>
